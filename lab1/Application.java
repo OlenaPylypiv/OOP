0d7a8c25019a67dcfbfdf2ee027a7b02eb84d0a9
olena.pylypiv16 <olena.pylypiv16@gmail.com>
2018-03-19 12:51:50 +0200

    my commit

---
+++ lab1/Application.java
@@ -0,0 +1,31 @@
+package com.tasks6.rle_decoder;
+
+public class Application
+{
+	public static void main( String[] args )
+	{
+		if(args[0] == null) {
+			System.out.println("error");
+			return;
+		}
+		if(args[0] == "") {
+			String k = "";
+			System.out.println(k);
+		}
+		
+		 StringBuilder dest = new StringBuilder(args[0]);
+         int count = 0;
+         for(int i = 0; i< dest.length(); i++) {
+        	 if(dest.charAt(i) == '0'  ) {
+        	 }
+        	 }
+         }
+		
+		
+		
+		
+		
+		
+		
+	} 
+}      
